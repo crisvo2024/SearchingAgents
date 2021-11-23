045afe066f057b842c797745db62396701dc7e52
Cristian Vargas <crvargaso@unal.edu.co>
2021-11-23 15:30:14 -0500

    successor function

--- main.py
+++ main.py
@@ -18,10 +18,14 @@ pieces = [
 def generate():
     board = np.zeros((10, 10))
     options = pieces.copy()
+    return fill(board, options)
+
+
+def fill(board: np.array, options: list):
     while options:
         selected = random.choice(options)
         options.remove(selected)
-        piece = pieces.index(selected)
+        piece = pieces.index(selected) + 1
         if random.randrange(2) == 1:
             selected = [selected[1], selected[0]]
         zeros = list(np.where(board == 0))
@@ -42,10 +46,36 @@ def generate():
     return board
 
 
+def successor(state: np.array):
+    board = state.copy()
+    used = np.unique(board)[1:]
+    print(used)
+    board[board == random.choice(used)] = 0
+    print(board)
+    used = np.unique(board)[1:]
+    print(used)
+    options = np.array(pieces)[~np.isin(np.arange(1, 11), used)]
+    print(options)
+    board = fill(board, options.tolist())
+    return board
+
+
+def heuristic(state):
+    return state[state == 0].size
+
+
+def hill_climbing():
+    pass
+
+
 def main():
     generated = generate()
     print(generated)
-    print(generated[generated == 0].size)
+    # heuristic function
+    print(heuristic(generated))
+    generated = successor(generated)
+    print(generated)
+    print(heuristic(generated))
 
 
 if __name__ == '__main__':
